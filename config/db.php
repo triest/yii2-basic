4c99ada9abc6212b5dc7700130e55d28c8f8a96c
triest <triest21@gmail.com>
2021-06-27 23:16:15 +0300

    First commit

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii_import',
+    'dsn' => 'mysql:host=localhost;dbname=yii-import',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
